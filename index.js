92bc7e284bee1d9e8c56097a3c8cc1ee83e1b30b
Shal Malhotra <smalhotra0109@gmail.com>
2023-09-25 18:47:01 -0400

    looping corrections

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = ["Charlie", "Samip", "Ali"];
+writeCards(names, "birthday");
+
+function writeCards(names, occasion) {
+    const thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const name = names[i];
+      const message = `Thank you, ${name}, for the wonderful ${occasion} gift!`;
+      thankYouMessages.push(message);
+    }
+  
+    return thankYouMessages;
+  }
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+
+
+
